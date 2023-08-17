6f19f39f5b2c75b964313b01ee24a3a50f34e0c7
bimsara22 <117551871+bimsara22@users.noreply.github.com>
2023-08-17 22:52:05 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+//        Pizza basePiza=new Pizza(true);
+//        basePiza.addExtraCheese();
+//        basePiza.getBill();
+
+
+        DeluxPizza dp=new DeluxPizza(true); //When this done True ,Not override methods when again added extra cheeses or toppings as Override methods in Delux pizza
+        dp.addExtraCheese();
+        dp.getBill();
+
+
+
+        }
+    }
---
+++ src/Pizza.java
@@ -0,0 +1,72 @@
+public class Pizza {
+
+    private int price;
+    private Boolean veg;
+
+    private boolean isExtraCheeseAdded=false;
+    private boolean isExtraToppingsAdded=false;
+    private boolean isOptedTakeaway=false;
+
+    private int extraCheese=100;
+    private int extraToppings=150;
+    private int backPack=20;
+    private int basePizzaPrice;
+
+    public Pizza(Boolean veg) {
+        this.veg = veg;
+        if (this.veg){
+            this.price=300;
+        }else {
+            this.price=400;
+        }
+        basePizzaPrice+=this.price;
+    }
+
+
+    public void getPizzaPrice(){
+        System.out.println(this.price);
+    }
+
+    public void addExtraCheese(){
+
+        isExtraCheeseAdded=true;
+        this.price+=extraCheese;
+
+
+    }
+    public void addExtraToppings(){
+        isExtraToppingsAdded=true;
+
+        this.price+=extraToppings;
+
+    }
+
+    public void takeAway(){
+        isOptedTakeaway=true;
+        this.price+=backPack;
+    }
+
+    public void getBill(){
+        String bill="";
+        System.out.println("Pizza : "+basePizzaPrice);
+
+        if (isExtraCheeseAdded){
+            bill+="Extra Cheese Added "+extraCheese+"\n";
+        }
+
+        if (isExtraToppingsAdded){
+            bill+="Extra Toppings Added :"+extraToppings+"\n";
+        }
+        if (isOptedTakeaway){
+            bill+="Take Away Added :"+backPack+"\n";
+        }
+
+        bill+="Bill "+this.price+"\n";
+        System.out.println(bill);
+
+    }
+
+
+
+
+}
